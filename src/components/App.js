37b8edaed3868b3c35ca11f79b22b18676786d80
UchihaOBIT0O <www.arun8790079239@gmail.com>
2024-02-05 19:57:33 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Logo from "./logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => id !== item.id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form handleAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        handleDeleteItem={handleDeleteItem}
+        handleToggleItem={handleToggleItem}
+        handleClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
